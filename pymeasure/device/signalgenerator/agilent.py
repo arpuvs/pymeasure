0853ae13635d00de552b1dc0e6b787e761feb577
Matsumoto Takao <taka031219@gmail.com>
2014-05-16 16:50:56 +0900

    sg.agilentのコード作成しました。

--- pymeasure/device/signalgenerator/agilent.py
+++ pymeasure/device/signalgenerator/agilent.py
@@ -3,13 +3,13 @@
 import signalgenerator as sg
 
 class agilent(sg.signalgenerator):
-    api = {'freq_set': '',
-           'freq_check': '',
-           'power_set': '',
-           'power_check': '',
-           'output_on': '',
-           'output_off': '',
-           'output_check': ''}
+    api = {'freq_set': 'FREQ %f %s\n' %(freq, unit),
+           'freq_check': 'FREQ?\n',
+           'power_set': 'POW %f dBm\n' %(power),
+           'power_check': 'POW?\n',
+           'output_on': 'OUTP 1\n',
+           'output_off': 'OUTP 0\n',
+           'output_check': 'OUTP?\n'}
 
 class E8257D(agilent):
     pass
@@ -18,5 +18,11 @@ class E8247C(agilent):
     pass
 
 class sg83732(agilent):
+    pass
+
+class E8241A(agilent):
+    pass
+
+class N5183A(agilent):
     pass
 
