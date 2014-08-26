b1f1f0b77a35d3ee049c9523b0d97f67c52ec741
Matsumoto Takao <taka031219@gmail.com>
2014-08-26 18:14:19 +0900

    pfeiffer.pyのモジュールを修正しました。

--- pymeasure/device/vacuummonitor/pfeiffer.py
+++ pymeasure/device/vacuummonitor/pfeiffer.py
@@ -4,7 +4,7 @@ __author__ = 'matsumototakao'
 
 import vacuummonitor as vm
 
-api = {'bar': 0,
+_unit = {'bar': 0,
        'torr': 1,
        'pascal': 2}
 
@@ -18,30 +18,19 @@ class pfeiffer(vm.vacuummonitor):
         self.com.open()
         self.com.send('PRX\n')
         ret = self.com.readline().strip().split(',')
-        del ret[1]
-        del ret[2]
-        press =str(ret)
+        press = [ret[1], ret[3]]
         self.com.close()
         return press
 
     def _unit_check(self):
-        self.com.open()
-        self.com.send('PRX\n')
-        ret = self.com.readline().strip().split(',')
-        del ret[0]
-        del ret[1]
-        unit = str(ret)
-        self.com.close()
-        return unit
+        return self.unit
 
     def _unit_set(self, unit):
         self.com.open()
-        if unit.lower()=='bar': self.com.send('UNI,%d'%(api['bar']))
-        elif unit.lower()=='torr': self.com.send('UNI,%d'%(api['torr']))
-        elif unit.lower()=='pascal': self.com.send('UNI,%d'%(api['pascal']))
-        else: print('unit is incorrect!')
+        self.com.send('UNI,%d'%(_unit[unit]))
+        self.unit = str(self.com.readline().strip())
         self.com.close()
-        return
+        return self.unit
 
 class D35614asslar(pfeiffer):
     pass
